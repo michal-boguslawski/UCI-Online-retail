0307fad23fbebd080d9264f1dcb1ba4fad99fd87
bogus <boguslawski.m.j@gmail.com>
2025-06-24 21:22:11 +0200

    Create PySpark logics to write data to S3

---
+++ data_transform/DimCancellation.py
@@ -0,0 +1,42 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+
+# Initialize Spark session
+spark = SparkSession.builder \
+    .appName("Read Avro from S3") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain") \
+    .getOrCreate()
+
+# S3 path to the Avro files
+s3_path = "s3a://bucket-uci-retail-project-1/bronze/kafka/sales_events/"
+
+# Read Avro data
+df = spark.read.format("avro").load(s3_path)
+
+df_flat = df.filter(
+    ( f.col("InvoiceNo").startswith("C") )
+).withColumn(
+    "CustomerId", 
+    f.when(f.isnan(f.col("CustomerID")) | f.col("CustomerID").isNull(), "Unknown")
+    .otherwise(f.col("CustomerID"))
+).select(
+    "InvoiceNo",
+    "InvoiceDate",
+    "Country",
+    "CustomerId",
+    f.lit("Kafka").alias("SourceSystem")
+)
+
+df_final = df_flat.withColumn("DimCustomerKey", f.hash(f.lit("Customer"), f.col("CustomerId")))
+df_final = df_final.withColumn("DimOrderKey", f.hash(f.lit("Cancellation"), f.col("InvoiceNo"), f.col("SourceSystem")))
+df_final.select(
+    "DimOrderKey",
+    "InvoiceNo",
+    "InvoiceDate",
+    "DimCustomerKey",
+    "Country",
+    "SourceSystem"
+)
+
+df_final.write.mode("overwrite").parquet("s3a://bucket-uci-retail-project-1/silver/dim_cancellation/v1")
